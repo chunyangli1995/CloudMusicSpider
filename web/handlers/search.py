609890949fee7584086ab224001dd17c6d8e1d51
chunyangli1995 <chunyangli1995@gmail.com>
2017-03-24 21:28:45 +0800

    add handler of user search

--- web/handlers/search.py
+++ web/handlers/search.py
@@ -1,8 +1,25 @@
 # coding=utf-8
 
+import requests
+
 import tornado.web
+
+pageSize = 11
+SEARCH_API_ADDR_OF_CLOUD_MUSIC = "http://music.163.com/api/search/pc"
+
 
 class SearchHandler(tornado.web.RequestHandler):
     
     def get(self):
-        type = self.get_argument("type")
+        type = int(self.get_argument("type"))
+        s = self.get_argument("s")
+        pageNum = int(self.get_argument("pageNum"))
+        offset = (pageNum - 1) * pageSize
+        payloads = {
+            "offset": offset,
+            "limit": pageSize,
+            "s": s,
+            "type": type
+        }
+        res = requests.post(SEARCH_API_ADDR_OF_CLOUD_MUSIC, params=payloads)
+        self.write(res.content)
